d79b6271a65af7dab4c075894b4017e570eb3265
rvdmolen <ricvdmolen@gmail.com>
2018-09-11 20:56:06 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AppHeaderComponent} from './components/app-header/app-header.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { RoomComponent } from './components/room/room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    RoomsComponent,
+    RoomComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/app-header/app-header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  styleUrls: ['app-header.component.scss'],
+  template: `
+    <div class="app-header">
+      <div class="wrapper">
+        <img src="/img/ing_logo.png">
+      </div>
+    </div>
+  `
+})
+export class AppHeaderComponent {
+
+}
